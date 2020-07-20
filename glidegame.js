0dde80be06d17e0663fb8ffae2c0224bd946bfce
O Blaufuss <49848589+bluef00t@users.noreply.github.com>
2020-07-19 23:08:19 -0400

    Fix buggy end-of-level behavior
    -Prevent minor wall clipping
    -Prevent escaping between-level bounding box
    -Added better coordinate system for level building

--- glidegame.js
+++ glidegame.js
@@ -18,14 +18,19 @@
         //Main game variables 
         var levelNum = 0;
         var side = 400;//EVERYTHING SCALES TO GAME SIZE
+        //display variables
         size(side, side); 
         frameRate(30);
         //drawing control variables
         var wallWidth = side/80;
         var meSpeed = side/50;
+        var endText = "";
 
-        var unit = side/10 + wallWidth;
+        var unit = (side - wallWidth*11)/10;
         var levelDone = false;
+
+        var endY = side/2 - unit;
+        var endHeight = 3*unit;
         
 
         //enum of directions, helpful for readable code
@@ -43,14 +48,10 @@
         var keysDown = [];
         var keyPressed = function() {
             setKey(keyCode, true);
-            //println("keyPressed = " + keyCode);
-            //println(isDown(keyCode));
         }
 
         var keyReleased = function() {
             setKey(keyCode, false);
-            //println("keyReleased = " + keyCode);
-            //println(isDown(keyCode));
         }
 
         var setKey = function(k, isDown) {
@@ -61,6 +62,24 @@
              if (k < KEYS_RANGE)  return keysDown[k];
         }
 
+        //TOOD: wallgrid and itemgrid functions to 
+        var wallGrid = function(coord){
+            return coord*(unit+wallWidth) + wallWidth/2;
+        }
+
+        var itemGrid = function(coord){
+            return (coord + 0.5)*(unit+wallWidth) + wallWidth/2;
+        }
+
+        // walls:[new Wall({x:1, y:side/2, height:side}),
+        //new Wall({x:side/2, y:1, height:side, isHoriz:true}), 
+        //new Wall({x:side-1, y:side/2, height:side}), 
+        //new Wall({x:side/2, y:side-1, height:side, isHoriz:true})]
+
+        //to
+
+        // walls:[new Wall({x:0, y:5, height:side}),
+
 
         var me = {
             x:side/2,
@@ -68,10 +87,9 @@
             width: unit*0.75,
             direction: moving.NO,
             draw: function(){
-                fill(255, 255, 0);
-                //console.log(this.x + this.width/2 + ", "+ this.y+ this.width/2+ ", " + this.width+ ", "+this.width);
                 //x and y represent center of smiley
                 //based on the KA default smiley; scales with width
+                fill(255, 255, 0);
                 stroke(0, 0, 0);
                 strokeWeight(2);
                 ellipse(this.x, this.y, this.width, this.width);
@@ -87,11 +105,19 @@
                 }
                 return false;
             },
-            handleCollisions: function(level){
-                //if touching wall, stop moving (and move to a position where no longer touching the wall, to enable future movement)
-                for(var i = 0; i<level.walls.length; i++){
-                    var thisWall = level.walls[i];
+            collectCoins: function(coins){//if touching a coin, collect it
+                 for(var i = 0; i<coins.length; i++){
+                    var thisCoin = coins[i];
+                    if(!thisCoin.collected && thisCoin.touchesMe()){
+                        thisCoin.collected = true;
+                    }
+                }
+            },
+            handleCollision: function(walls){//if touching wall, stop moving (and move to a position where no longer touching the wall, to enable future movement)
+                for(var i = 0; i<walls.length; i++){
+                    var thisWall = walls[i];
                     while(thisWall.touchesMe()){
+                        //println("touching me, now going " + this.direction);
                         var tolerance = 2; //how close to get to the wall
                         switch(this.direction){//inch away until fixed
                             case moving.UP: me.y = me.y +tolerance; break;
@@ -105,10 +131,46 @@
                         }
                     }
                 }
-                //if touching finish line, trigger win condition
-                if(me.touchesEnd(level)){
-                    println("hooray!");
+            },
+            update: function(level){
+                this.handleCollision(level.walls);
+                this.collectCoins(level.coins);
+                if(this.touchesEnd(level)){//trigger win condition
+                    levelDone = true;
+                    me.direction = moving.NO;
+                    me.x = side/2;
+                    me.y = side/2;
+                    var n = 0;
+                    for (var i = 0; i<level.coins.length; i++){
+                        if (level.coins[i].collected) n++;
+                    }
+                    textSize(unit/2);
+                    text("< BACK", wallWidth, endY-unit, unit*2, unit);
+                    text("NEXT >", side-unit*2, endY-unit, unit*2, unit);
+                    endText = "You collected " + n + " out of " + level.coins.length + " coins.";
                 }
+            }
+        }
+
+        var Coin = function(config){
+            this.collected = false;
+            this.x = config.x || unit*5;
+            this.y = config.y || unit*5;
+            this.coinSize = unit/2;
+            this.touchesMe = function(){
+                var r = me.width/2 + this.coinSize/2;
+                //println(dist(me.x, me.y, this.x, this.y) + ", " + r);
+                if(dist(me.x, me.y, this.x, this.y) < r){
+                    return true;
+                }
+                return false;
+            };
+            this.draw = function(){
+                fill(255, 255, 0);
+                stroke(0, 0, 0);
+                strokeWeight(2);
+                ellipse(this.x, this.y, this.coinSize * 0.7, this.coinSize);
+                ellipse(this.x - unit/10, this.y, this.coinSize * 0.7, this.coinSize);
             }
         }
 
@@ -121,12 +183,10 @@
             //returns boolean: true if smiley is touching wall
             this.touchesMe = function(){
                 var heightR = this.height/2 + me.width/2;
-                var widthR = wallWidth/2 + me.width/2;
+                var widthR = wallWidth/2+ me.width/2;
                 if (this.isHoriz){//calculation of distance from wall horizontal
                     if (me.x < this.x + heightR &&
                         me.x > this.x - heightR){//if inside the X plane of the line
-                        //println("within x  of horizontal line");
-                        //println(dist(0, me.y, 0, this.y) + ", " + r);
                         if (dist(0, me.y, 0, this.y) < widthR){
                             return true;
                         }
@@ -157,6 +217,7 @@
             }
         }
 
+        //TODO: level builder interface
         var Level = function(config){
             this.coins = config.coins || [];
             this.startX = config.startX || unit/2;
@@ -170,7 +231,10 @@
             this.init = function(){
                 me.x = this.startX;
                 me.y = this.startY;
-            }
+                for (var i = 0; i < this.coins.length; i++){
+                    this.coins[i].collected = false;
+                }
+            };
             
             this.draw = function(){
                 background(180,180,180);
@@ -178,6 +242,10 @@
                 rectMode(CENTER);
                 for(var i=0; i < this.walls.length; i++){
                     this.walls[i].draw();
+                }
+                //render coins
+                for(var i=0; i < this.coins.length; i++){
+                    if (!this.coins[i].collected) this.coins[i].draw();
                 }
                 //render end box
                 rectMode(CORNER);
@@ -208,20 +276,26 @@
         /*
          walls:[new Wall({x:1, y:side/2, height:side}), new Wall({x:side/2, y:1, height:side, isHoriz:true}), new Wall({x:side-1, y:side/2, height:side}), new Wall({x:side/2, y:side-1, height:side, isHoriz:true})]
          */
+         //TODO: put x and y into arrays, automatically populate levels when created
         var L1 = new Level({
             walls:[new Wall({x:1, y:side/2, height:side}), new Wall({x:side/2, y:1, height:side, isHoriz:true}), new Wall({x:side-1, y:side/2, height:side}), new Wall({x:side/2, y:side-1, height:side, isHoriz:true}),
             new Wall({x:side/2, y:unit, height:2*unit}), new Wall({x:side/2, y:side-unit, height:2*unit}), 
             new Wall({y:side/2, x:unit, height:2*unit, isHoriz:true}), new Wall({y:side/2, x:side-unit, height:2*unit, isHoriz:true}),
             new Wall({y:side/2 + unit, x:side/2, height:2*unit, isHoriz:true})
-            ]
+            ],
+            coins:[new Coin({x:itemGrid(0), y:itemGrid(1)}), new Coin({x:itemGrid(0), y:itemGrid(2)}), new Coin({x:itemGrid(0), y:itemGrid(3)})]
         });
         L1.init();
         L1.draw();
         me.draw();
 
+        //define all levels, walls, etc
+        var endWalls = [new Wall({isHoriz: true, x:side/2, y:endY}), new Wall({isHoriz: true, x:side/2, y:endY+endHeight})];
+
+
+
         //every frame drawn here
         draw = function(){
-            //println(keyCode + ", " + isDown(keyCode));
             if (keyIsDown(keyCode) && me.direction === moving.NO){
                 switch (keyCode){
                     case UP: me.direction = moving.UP; break;
@@ -236,14 +310,34 @@
                  case moving.DOWN: me.y = me.y +meSpeed; break;
                   case moving.LEFT: me.x = me.x -meSpeed; break;
                    case moving.RIGHT: me.x = me.x +meSpeed; break;
-            }
+                }
             if (!levelDone){
                 L1.draw();
-                me.handleCollisions(L1);
-            }else{
-
+                me.update(L1);
+                me.draw();
+            }else{// level conclusion
+                //draw results box
+                rectMode(CORNER);
+                fill(240, 240, 240);
+                stroke(0, 0, 0);
+                strokeWeight(unit/10);
+                rect(0, endY, side, endHeight);
+                fill(0,0,0);
+                textSize(unit/2);
+                text(endText, endHeight, endY+unit, side, endHeight);
+                me.handleCollision(endWalls);
+                me.draw();
+                //two option choices
+                if (me.x < 0){//replay level
+                    L1.init();
+                    levelDone = false;
+                }else if(me.x > side){
+                    println(me.x);
+                    //go to next level
+                    levelDone =  false;
+                }
             }
-            me.draw();
+            
         }
     }};
 
